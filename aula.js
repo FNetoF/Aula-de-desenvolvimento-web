c854f1de32462eed571382e7db2571eb8c9d6f2e
FNetoF <netofelipeferreira@hotmail.com>
2022-08-25 10:18:14 -0300

    Aula do dia 25/ago

---
+++ aula.js
@@ -0,0 +1,18 @@
+let variavel = 'Felipe Neto'
+
+console.log('Nome: ' + variavel)
+
+variavel = 1999
+
+console.log('Idade: ' + variavel)
+
+let inventario = {
+    camisa: 55.00,
+    tenis: 150.50,
+    meia: 9.90
+}
+
+console.log(inventario)
+console.log('Preço da camisa R$ ' + inventario.camisa + ' reais')
+console.log('Preço do Tênis R$ ' + inventario.tenis + ' reais')
+console.log('Preço da meia R$ ' + inventario.meia + ' reais')
